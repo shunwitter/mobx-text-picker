980ed3d0a09b226a87c538783edb83f733bb89d0
shunwitter <polytechnics.shun@gmail.com>
2017-06-29 21:22:55 +0900

    Move components and store from private repo

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-var MobxTextPicker = require('./lib/MobxTextPicker');
+var MobxTextPicker = require('./lib/components/MobxTextPicker');
+var MobxTextPickerStoreBase = require('./lib/store/MobxTextPickerStoreBase');
 
 module.exports = MobxTextPicker;
+module.exports = MobxTextPickerStoreBase;
